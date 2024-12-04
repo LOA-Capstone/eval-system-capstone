d45258c70b732ed481e21e536c1800c11921a596
itsmeenavi <105837329+itsmeenavi@users.noreply.github.com>
2024-12-04 09:51:07 +0800

    Added school ID on login, and removed view in user list

--- admin/user_list.php
+++ admin/user_list.php
@@ -50,8 +50,7 @@
                               Action
                             </button>
                             <div class="dropdown-menu" style="">
-                              <a class="dropdown-item view_user" href="javascript:void(0)" data-id="<?php echo $row['id']; ?>">View</a>
-                              <div class="dropdown-divider"></div>
+                           
                               <a class="dropdown-item" href="./index.php?page=edit_user&id=<?php echo $row['id']; ?>">Edit</a>
                               <div class="dropdown-divider"></div>
                               <a class="dropdown-item delete_user" href="javascript:void(0)" data-id="<?php echo $row['id']; ?>">Delete</a>
